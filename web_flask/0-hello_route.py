9cad0c884f0ab8a9e824e7a23b62540171f43525
samueltezera28 <samueltezera28@gmail.com>
2022-10-13 11:02:19 +0300

    hello_route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" starts a flask web application """
+from flask import Flask
+app =Flask(__name__)
+
+
+@app.route('/',strict_slashes=False)
+def hello_hbnb():
+    """ prints a message when / is called """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
+
+
